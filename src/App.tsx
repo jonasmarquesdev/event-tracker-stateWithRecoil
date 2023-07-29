cc4180cb2bbd6817a8bab51f8188017572724d89
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-07-28 21:19:21 -0300

    fix: comentários

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,19 @@ function App() {
 
   const [filtro, setFiltro] = useState<Date | null>()
 
-  const adicionarEvento = (evento: IEvento) => {
-    evento.id = Math.round((new Date()).getTime() / 1000)
-    eventos.push(evento)
-    console.log(eventos);
+  // const adicionarEvento = (evento: IEvento) => {
+  //   evento.id = Math.round((new Date()).getTime() / 1000)
+  //   eventos.push(evento)
+  //   console.log(eventos);
     
-    setEventos([...eventos])
-  }
+  //   setEventos([...eventos])
+  // }
   const alterarStatusEvento = (id: number) => {
-    const evento = eventos.find(evento => evento.id === id)
-    if (evento) {
-      evento.completo = !evento.completo
-    }
-    setEventos([...eventos])
+    // const evento = eventos.find(evento => evento.id === id)
+    // if (evento) {
+    //   evento.completo = !evento.completo
+    // }
+    // setEventos([...eventos])
   }
   // const deletarEvento = (id: number) => {
   //   setEventos([...eventos.filter(evento => evento.id !== id)])
@@ -36,10 +36,10 @@ function App() {
   }
 
   const filtrados = !filtro
-    ? eventos
-    : eventos.filter((evento) =>
-      filtro!.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
-    );
+    // ? eventos
+    // : eventos.filter((evento) =>
+    //   filtro!.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
+    // );
 
   return (
     <RecoilRoot>
